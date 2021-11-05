9abd464131574e960a8fc90484d714a15384d7ce
Mateo Contenla <mcontenlaf@gmail.com>
2021-11-05 12:04:06 -0300

    Adición de script para servidor web

--- src/arduino/include/config.h
+++ src/arduino/include/config.h
@@ -1,12 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define WIFI_SSID "MCF"
-#define WIFI_PASS "hola1234"
-#define SERVER_LOCAL_IP "192.168.43.35"
-#define SERVER_LOCAL_PUERTO "8000"
+#define WIFI_SSID "Parasitos"
+#define WIFI_PASS "homero2040"
+#define SERVER_LOCAL_IP "192.168.1.220"
+#define SERVER_LOCAL_PUERTO "80"
 
-#define DEBUG true
+#define DEBUG false
 #define DEBUG_SERIAL if(DEBUG)Serial
 
 #endif
---
+++ src/servidor/reset_web_files.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mv -v ./* /var/www/html/
